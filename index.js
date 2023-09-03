cbb94524009ca75d9f9e2d849f61bfd11471b506
Ryuofficial <104766474+Ryuofficial@users.noreply.github.com>
2023-09-04 06:49:27 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const dlBtn = document.getElementById("redirectButton");
+const playBtn = document.getElementById("redirectButton2");
+
+// Add a click event listener to the button
+dlBtn.addEventListener("click", function() {
+  // Redirect to the desired URL
+  window.location.href = "https://play.google.com/store/apps/details?id=com.ryumoto.gfx&pli=1";
+});
+
+playBtn.addEventListener("click", function() {
+  // Redirect to the desired URL
+  window.location.href = "https://play.google.com/store/apps/details?id=com.ryumoto.gfx&pli=1";
+});
