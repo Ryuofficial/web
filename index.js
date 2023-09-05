16bfe8bf9afcef7282b5d1ac3737e98aba1a098f
Ryuofficial <104766474+Ryuofficial@users.noreply.github.com>
2023-09-05 17:05:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const playBtn = document.getElementById("redirectButton2");
 // Add a click event listener to the button
 dlBtn.addEventListener("click", function() {
   // Redirect to the desired URL
-  window.location.href = "https://github.com/Ryuofficial/app/raw/main/RyumotoGFX.apk";
+  window.location.href = "https://www.mediafire.com/file/ivbnypik5qna523/RyumotoGFX.apk/file";
 });
 
 playBtn.addEventListener("click", function() {
